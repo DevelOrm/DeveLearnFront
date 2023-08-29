f6be648d30eececcd24af2b6dcc15fe378db836d
주기현 <0318joo@naver.com>
2023-08-29 16:29:37 +0900

    ♻️ refactor: JQuery문법을 es6문법으로 변경

--- assets/js/classroom-detail.js
+++ assets/js/classroom-detail.js
@@ -1,8 +1,8 @@
 const classroomDetailURL = "http://3.37.187.68:8000/classroom/detail";
-const classroomNames = $(".classroom-name");
-const classroomInfo = $(".classroom-info");
-const classroomTag = $(".classroom-tag");
-const classroomDate = $(".classroom-date");
+const classroomNames = document.querySelectorAll(".classroom-name");
+const classroomInfo = document.querySelectorAll(".classroom-info");
+const classroomTag = document.querySelectorAll(".classroom-tag");
+const classroomDate = document.querySelectorAll(".classroom-date");
 
 function getPKFromQuery() {
   const urlParams = new URLSearchParams(window.location.search);
@@ -10,16 +10,21 @@ function getPKFromQuery() {
 }
 
 function setClassroomName(name) {
-  for (let index = 0; index < classroomNames.length; index++) {
-    classroomNames[index].innerText = name;
-  }
+  classroomNames.forEach(element => {
+    element.innerText = name;
+  });
 }
 
-$(document).ready(function () {
-  $.get(`${classroomDetailURL}/${getPKFromQuery()}/`, function (data) {
-    setClassroomName(data.class_name);
-    classroomInfo[0].innerText = data.class_info;
-    classroomTag[0].innerText = data.tag.join(" ");
-    classroomDate[0].innerText = data.created_at;
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  fetch(`${classroomDetailURL}/${getPKFromQuery()}/`)
+    .then(response => response.json())
+    .then(data => {
+      setClassroomName(data.class_name);
+      classroomInfo[0].innerText = data.class_info;
+      classroomTag[0].innerText = data.tag.join(" ");
+      classroomDate[0].innerText = data.created_at;
+    })
+    .catch(error => {
+      console.error("Error fetching data:", error);
+    });
 });
